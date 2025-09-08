1a77a5d39a18434bc5a2b1334f1f98108485a2bb
garagoz <atorayew@gmail.com>
2025-09-08 21:21:31 +0500

    project updated

--- src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/bankcards/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.bankcards.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
